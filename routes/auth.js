39069e2f758a13c772d0ce51927d409b42b36c4c
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-13 22:04:34 -0500

    auth

---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const knex = require("knex")(require("../knexfile"));
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+
+router.post("/login", async (req, res) => {
+    // get credentials from the req.body
+    const {username, password} = req.body;
+
+    // find the user by username 
+    const user = await knex("users")
+        .where({ email: username })
+        .first();
+    
+    // invalid login
+    if (!user) {
+        return res.status(403).send("Combination not found");
+    }
+
+    // Validate the password
+    const passwordMatching = user.password === password;
+
+    if (!passwordMatching) {
+        return res.status(403).send("Combination of username & password not found"); 
+    }
+
+    //generating the token
+    const token = jwt.sign(
+        {
+            id: user.id,
+            nickname: user.nickname
+        },
+        process.env.SECRET_KEY
+    );
+
+
+    res.status(200).json({
+        access_token: token,
+    });
+});
+
+module.exports = router;
--- routes/vocabularyRoutes.js
+++ routes/vocabularyRoutes.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const router = express.Router();
 
+
 app.use(cors());
 require("dotenv").config();
 const knex = require("knex")(require("../knexfile"));
