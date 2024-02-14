06ed05a8606d1df4131a224a8973ffde32086d6c
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-13 20:51:05 -0500

    get and post vocabulary api created

---
+++ migrations/20240214013440_remove_date_from_words.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('words', function (table) {
+        table.dropColumn('date');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('words', function (table) {
+        table.date('date');
+      });
+};
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const knex = require("knex")(require("./knexfile"));
 const cors = require("cors");
 const usersRoutes = require("./routes/usersRoutes.js")
 const vocabularyRoutes = require("./routes/vocabularyRoutes.js")
 const trainingsRoutes = require("./routes/trainingsRoutes.js")
 
 const PORT = process.env.PORT || 8081;
-const CLIENT_URL = process.env.CLIENT_URL;
 
 app.use(cors());
-
-app.use(cors({ origin: CLIENT_URL }));
-
-app.use("/users", usersRoutes);
-app.use("/vocabulary", vocabularyRoutes);
-app.use("/trainings", trainingsRoutes);
-
 app.use(express.json());
+
+// app.use("/users", usersRoutes);
+app.use("/vocabulary", vocabularyRoutes);
+// app.use("/trainings", trainingsRoutes);
+
+
 
 
 
