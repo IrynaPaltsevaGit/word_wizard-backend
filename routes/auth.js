f3081c43573c73658c8869f191c3b77bc2873cf3
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-14 22:55:21 -0500

    signup added

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,32 @@ const knex = require("knex")(require("../knexfile"));
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
+router.post("/signup", async (req, res) => {
+    
+    try{ 
+        const {username, password, nickname} = req.body;
+        console.log(req.body)
+        // Insert the new user into the 'users' table
+        const [newUserId] = await knex("users")
+            .insert({
+                email: username,
+                password,
+                nickname
+            }); 
+        // Fetch the newly added user from the database
+        const newUser = await knex('users')
+            .where('id', newUserId)
+            .first();
+
+        // Send the newly added user as a JSON response
+        res.status(201).json(newUser);
+    }catch(error){
+        res.status(400).json(`signup catch, sys.error: ${error}`);
+    }
+});
+
+
+
 router.post("/login", async (req, res) => {
     // get credentials from the req.body
     const {username, password} = req.body;
