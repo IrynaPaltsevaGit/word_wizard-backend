b6b85ab3f4a9d61475b2da9a59b96bfae4dfd836
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-17 22:07:16 -0500

    id in routed added

--- routes/trainingsRoutes.js
+++ routes/trainingsRoutes.js
@@ -3,7 +3,34 @@ const cors = require("cors");
 const app = express();
 const router = express.Router();
 
-app.use(cors());
-require("dotenv").config();
 const knex = require("knex")(require("../knexfile"));
-module.exports = router;
+const authMiddleware = require('../middlewares/authorize.middleware.js');
+require("dotenv").config();
+
+app.use(cors());
+
+// getting 10 the word cards for the user
+router.get("/", authMiddleware, async (req, res) => {
+    try {
+     const data = await knex("words")
+       .select(
+         "words.id",
+         "words.word",
+         "words.translation",
+         "words.progress"
+       )
+       .join("users", "words.user_id", "users.id")
+       .where({ 'users.id': req.userObj.id})
+       .orderBy("words.progress", "asc")
+       .limit(10);
+       
+       res.status(200).json(data);
+     
+   } catch  (error) {
+     console.log(error)
+     res.status(400).json(error);
+     
+   }
+ });
+
+ module.exports = router;
