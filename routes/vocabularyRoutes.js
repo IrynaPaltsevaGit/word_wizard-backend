9c7974d977b88f76360ce897a390a7dc7c2b96c6
IrynaPaltsevaGit <irynapaltseva@gmail.com>
2024-02-19 22:38:20 -0500

    offset added

--- routes/vocabularyRoutes.js
+++ routes/vocabularyRoutes.js
@@ -10,26 +10,39 @@ app.use(cors());
 
 // getting all the word cards for the user
 router.get("/", authMiddleware, async (req, res) => {
-     try {
-      const data = await knex("words")
+    const {limit, page} = req.query;
+    const offset = page * limit - limit;
+    console.log(offset)
+
+    try {
+      const words = await knex("words")
         .select(
           "words.id",
           "words.word",
           "words.translation",
           "words.progress",
-          "words.notes"
+          "words.notes",
+          "words.created_at"
         )
         .join("users", "words.user_id", "users.id")
-        .where({ 'users.id': req.userObj.id});
+        .where({ 'users.id': req.userObj.id})
+        .orderBy(["words.progress", "words.created_at"], "asc")
+        .limit(limit)
+        .offset(offset);
+      
+      const response = await knex("words")
+        .join("users", "words.user_id", "users.id")
+        .where({ 'users.id': req.userObj.id})
+        .count();
         
-        res.status(200).json(data);
+        res.status(200).json({words, wordsAmount: response[0]['count(*)']});
       
     } catch  (error) {
       console.log(error)
       res.status(400).json(error);
       
     }
-  });
+});
 
 // POST route to create a new word card
 router.post("/new", authMiddleware, async (req, res) => {
